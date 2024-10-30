ce8c68e853a41eaf1f73b9f8108b87e056e58597
Thunder <49893401+ThunderDZNGITH@users.noreply.github.com>
2024-10-30 09:35:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,83 +15,95 @@
 #include <vector>
 
 using namespace std;
-
-// Représente un compte bancaire avec son id unique, son passcode, et son solde
+/**
+	account (struct)
+	Représente un compte bancaire avec son id unique, son passcode, et son solde.
+**/
 struct account {
-	long long id; 	// Id unique en 16 chiffres, ex : 8800 0000 1234 5678
-	int passcode; 	// Passcode en 4 chiffres
-	int amount; 	// Solde
+	long long id; 													// Id unique en 16 chiffres, ex : 8800 0000 1234 5678.
+	int passcode; 													// Passcode en 4 chiffres.
+	int amount; 													// Solde.
 };
 
-// Types de transactions
+/**
+	transactionType (enum)
+	Énumération du type de transaction
+**/
 enum transactionType {
-	deposit = 1, 	// Dépot
-	withdraw = 2, 	// Retrait
-	transfer = 3	// Transfert
+	deposit = 1, 													// Dépot.
+	withdraw = 2, 													// Retrait.
+	transfer = 3													// Transfert.
 };
 
-// Représente une transaction bancaire avec son id unique, l'id de l'émetteur, l'id du récepteur, le type de transaction et son montant
+/**
+	transaction (struct)
+	Représente une transaction bancaire avec son id unique, l'id de l'émetteur, l'id du récepteur, le type de transaction et son montant.
+**/
 struct transaction {
-	long long id;			// Id unique de la transaction en 16 chiffres, ex : 4444 0000 1234 5678
-	long long fromId;		// Id de l'émetteur
-	long long toId;			// Id du récepteur
-	transactionType type;	// Type de transaction (deposit, withdraw, transfer)
-	int amount;				// Montant
+	long long id;													// Id unique de la transaction en 16 chiffres, ex : 4444 0000 1234 5678.
+	long long fromId;												// Id de l'émetteur.
+	long long toId;													// Id du récepteur.
+	transactionType type;												// Type de transaction (deposit, withdraw, transfer).
+	int amount;													// Montant.
 };
 
-unsigned short int choice = 0;	// Choix du menu
+unsigned short int choice = 0;												// Choix du menu.
 bool doExit = false;
 
-account actualAccount; 	// Compte local actuel
+account actualAccount; 													// Compte local actuel.
 
-const string ACCOUNTDATABASE("ACCOUNTDATABASE.txt");			// Base de données des comptes
-const string TRANSACTIONDATABASE("TRANSACTIONDATABASE.txt");	// Base de données des transactions
+const string ACCOUNTDATABASE("ACCOUNTDATABASE.txt");									// Base de données des comptes.
+const string TRANSACTIONDATABASE("TRANSACTIONDATABASE.txt");								// Base de données des transactions.
 
-// Actions du menu
+/**
+	menuChoice (enum)
+	Énumération des actions du menu.
+**/
 enum menuChoice {
-    createAccount = 1,	// Créer un compte
-	connectAccount =2,	// Connecter un compte
-    dropMoney = 3,		// Déposer de l'argent
-    takeMoney = 4,		// Retirer de l'argent
-    transferMoney = 5,	// Transferer de l'argent vers un autre compte
-    checkAmount = 6,	// Vérifier le solde du compte
-    checkHistory = 7,	// Vérifier l'histroique des transactions du compte
-    closeAccount = 8,	// Fermer un compte
-    close = 9			// Terminer le programme
+    createAccount = 1,													// Créer un compte.
+	connectAccount =2,												// Connecter un compte.
+    dropMoney = 3,													// Déposer de l'argent.
+    takeMoney = 4,													// Retirer de l'argent.
+    transferMoney = 5,													// Transferer de l'argent vers un autre compte.
+    checkAmount = 6,													// Vérifier le solde du compte.
+    checkHistory = 7,													// Vérifier l'histroique des transactions du compte.
+    closeAccount = 8,													// Fermer un compte.
+    close = 9														// Terminer le programme.
 };
 
+/**
+	saveAccount (void)
+	Paramètre : acc (account)
+	Enregistrer les paramètres d'un compte dans la base de données.
+**/
 void saveAccount(account acc) {
     std::ifstream iFile(ACCOUNTDATABASE);
     std::ofstream oFile("temp.txt");
     std::string line;
     bool found = false;
+	
+    std::vector<std::string> lines;											// Vecteur pour stocker les lignes du fichier.
 
-    // Vecteur pour stocker les lignes du fichier
-    std::vector<std::string> lines;
-
-    // Lire toutes les lignes du fichier existant
-    while (getline(iFile, line)) {
+    while (getline(iFile, line)) {    											// Lire toutes les lignes du fichier existant.
         lines.push_back(line);
     }
 
     iFile.close();
 
-    // Parcourir les lignes et vérifier si l'ID existe déjà
-    for (size_t i = 0; i < lines.size(); ++i) {
+    for (size_t i = 0; i < lines.size(); ++i) {										// Parcourir les lignes et vérifier si l'ID existe déjà.
         if (lines[i] == std::to_string(acc.id)) {
-            // Écraser les trois lignes si l'ID est trouvé
+															// Écraser les trois lignes si l'ID est trouvé.
             oFile << acc.id << std::endl;
             oFile << acc.id << "+P+" << acc.passcode << std::endl;
             oFile << acc.id << "+A+" << acc.amount << std::endl;
             found = true;
-            i += 2; // Sauter les deux lignes suivantes qui correspondent à l'ancienne entrée
+            i += 2; 													// Sauter les deux lignes suivantes qui correspondent à l'ancienne entrée.
         } else {
             oFile << lines[i] << std::endl;
         }
     }
 
-    // Si l'ID n'est pas trouvé, ajouter les informations à la fin
-    if (!found) {
+    if (!found) { 													// Si l'ID n'est pas trouvé, ajouter les informations à la fin.
         oFile << acc.id << std::endl;
         oFile << acc.id << "+P+" << acc.passcode << std::endl;
         oFile << acc.id << "+A+" << acc.amount << std::endl;
@@ -99,11 +111,16 @@ void saveAccount(account acc) {
 
     oFile.close();
 
-    // Remplacer le fichier original par le fichier temporaire
+															// Remplacer le fichier original par le fichier temporaire.
     remove("ACCOUNTDATABASE.txt");
     rename("temp.txt", "ACCOUNTDATABASE.txt");
 }
 
+/**
+	displayMenu (void)
+	Paramètre:
+	Afficher le menu de navigation.
+**/
 void displayMenu() {
 	cout << "Votre compte est actuellement le : " << actualAccount.id << endl;
 	cout << "Vous avez : " << actualAccount.amount << "€" << endl;
@@ -122,36 +139,56 @@ void displayMenu() {
     cout << "Quel est votre choix ?" << endl;
 }
 
+/**
+	generateBankId (long long)
+	Paramètre:
+	Génère un ID bancaire unique sous le format 88XXXXXXXXXXXXXX.
+**/
 long long generateBankId() {
     long long base = 8800000000000000;
     long long randomPart = rand() % 100000000000000;
     return base + randomPart;
 }
 
+/**
+	generateTransactionId (long long)
+	Paramètre:
+	Génère un ID de transaction unique sous le format 4444XXXXXXXXXXXX.
+**/
 long long generateTransactionId() {
     long long base = 4444000000000000;
     long long randomPart = rand() % 100000000000000;
     return base + randomPart;
 }
 
+/**
+	createAccountFunc (void)
+	Paramètre:
+	Prompt de création de compte.
+**/
 void createAccountFunc() {
 	int passcode;
 	int amount;
 	account acc;
-	cout << "Quel est votre passcode ?" << endl;
+	cout << "Quel est votre passcode ?" << endl;									//	Demande du passcode désirer.
 	cin >> passcode;
 	acc.passcode = passcode;
-	cout << "Quel est votre solde de départ ?" << endl;
+	cout << "Quel est votre solde de départ ?" << endl;								//	Demande du solde de départ.
 	cin >> amount;
 	acc.amount = amount;
 	
 	srand(time(0));
-	acc.id = generateBankId();
+	acc.id = generateBankId();											//	Génèration d'un ID bancaire unique.
 	
-	saveAccount(acc);
+	saveAccount(acc);												//	Sauvegarde des paramètres du compte.
 	cout << "Votre compte est le : " << acc.id << " | Avec le code : " << passcode << endl;
 }
 
+/**
+	connectAccountFunc (void)
+	Paramètre:
+	Prompt de connexion au compte.
+**/
 void connectAccountFunc() {
 	long long id = 0;
 	string passcode;
@@ -167,7 +204,7 @@ void connectAccountFunc() {
 	
 	string lineid;
 	string linepasscode;
-	while(getline(iFile, lineid)) {
+	while(getline(iFile, lineid)) {											//	Vérification de l'existance du compte.
 		if(lineid.find(id)) {
 			accountFind = true;
 			cout << "Compte trouvé ! " << endl;
@@ -176,9 +213,8 @@ void connectAccountFunc() {
 			cout << "Compte introuvable !" << endl;	
 		}
 	}
-	iFile.close();
-	iFile.open(ACCOUNTDATABASE);
-	if(accountFind) {
+	
+	if(accountFind) {												//	Si le compte est trouvé, vérifier la correspondance du passcode.
 		cout << "Quel est votre passcode ?" << endl;
 		cin >> passcode;
 		while(getline(iFile, linepasscode)) {
@@ -188,10 +224,11 @@ void connectAccountFunc() {
 			}
 		}
 	}
-	if(passcodeCorrect) {
-		actualAccount.id = id;
+	
+	if(passcodeCorrect) {												//	Si le passcode est correct, retrouver le solde (amount) du compte.
+		actualAccount.id = id;											//	Définir l'id de actualAccount à l'id du compte en cours de connexion.
 		cout << "Vous êtes connécté au compte : " << actualAccount.id << endl;
-		actualAccount.passcode = stoi(passcode);
+		actualAccount.passcode = stoi(passcode);								//	Définir le passcode de actualAccount au passcode du compte en cours de connexion.
 		
 		string lineAmount = to_string(id)+"+A+";
 		unsigned int currentAmount = 0;
@@ -203,7 +240,7 @@ void connectAccountFunc() {
 				if (pos != string::npos) {
 					string number = line.substr(pos + 1);
 					currentAmount = stoi(number);
-					actualAccount.amount = currentAmount;
+					actualAccount.amount = currentAmount;						//	Définir le solde (amount) de actualAccount au solde du compte en cours de connexion.
 				}
 				break;
 			}
@@ -214,14 +251,19 @@ void connectAccountFunc() {
 		iFile.close();
 }
 
+/**
+	generateTransaction (void)
+	Paramètre: tra (transaction)
+	Génère une trace d'une transaction dans TRANSACTIONDATABASE.
+**/
 void generateTransaction(transaction tra) {
 	ofstream oFile(TRANSACTIONDATABASE, ios::app);
 	
 	srand(time(0));
-	tra.id = generateTransactionId();
+	tra.id = generateTransactionId();										//	Génèrer un id de transaction.
 	
 	string type;
-	switch(tra.type){
+	switch(tra.type){												//	En fonction du type de transaction, inscrire son label (1 -> deposit, 2 -> withdrawal, 3 -> transfer).
 		case 1:
 			type = "deposit";
 			break;
@@ -233,15 +275,21 @@ void generateTransaction(transaction tra) {
 			break;
 	}
 	
+															//	Inscription de la transaction dans la TRANSACTIONDATABASE.
 	oFile << tra.id << " | From : " << tra.fromId << " To : " << tra.toId << " | " << type << " | " << tra.amount  << "€" << endl;
 	
 	oFile.close();
 }
 
+/**
+	addMoney (void)
+	Paramètres: amount (int), acc (account)
+	Ajoute 'amount' d'argent sur le compte 'acc'.
+**/
 void addMoney(int amount, account acc) {
 	ifstream iFile(ACCOUNTDATABASE);
 	
-	string lineAmount = to_string(acc.id)+"+A+";
+	string lineAmount = to_string(acc.id)+"+A+";									//	Recherche du solde du compte 'acc'.
 	unsigned int currentAmount = 0;
 	
 	string line;
@@ -251,19 +299,24 @@ void addMoney(int amount, account acc) {
     		if (pos != string::npos) {
         		string number = line.substr(pos + 1);
 				currentAmount = stoi(number);
-				acc.amount = currentAmount+amount;
+				acc.amount = currentAmount+amount;							// Ajouter 'amount' au solde de 'acc'
 			}
 			break;
 		}
 	}
 	
-	saveAccount(acc);
+	saveAccount(acc);												//	Enregistrer les paramètres du compte.
 }
 
+/**
+	subMoney (void)
+	Paramètres: amount (int), acc (account)
+	Retire 'amount' d'argent sur le compte 'acc'.
+**/
 void subMoney(int amount, account acc) {
 	ifstream iFile(ACCOUNTDATABASE);
 	
-	string lineAmount = to_string(acc.id)+"+A+";
+	string lineAmount = to_string(acc.id)+"+A+";									//	Recerche du solde du compte 'acc'.
 	unsigned int currentAmount = 0;
 	
 	string line;
@@ -273,22 +326,27 @@ void subMoney(int amount, account acc) {
     		if (pos != string::npos) {
         		string number = line.substr(pos + 1);
 				currentAmount = stoi(number);
-				acc.amount = currentAmount-amount;
+				acc.amount = currentAmount-amount;							//	Retirer 'amount' du solde de 'acc'.
 			}
 			break;
 		}
 	}
 	
-	saveAccount(acc);
+	saveAccount(acc);												//	Enregistrer les paramètres du compte.
 }
 
+/**
+	dropMoneyFunc (void)
+	Paramètre:
+	Prompt de dépot d'argent.
+**/
 void dropMoneyFunc() {
 	transaction tra;
 	
 	int amount = 0;
-	if(actualAccount.id == 0) {
-		cout << "Connectez vous à un compte d'abord." << endl;	
-	} else {
+	if(actualAccount.id == 0) {											//	Si aucun compte connecté : Erreur.
+		cout << "Connectez vous à un compte d'abord." << endl;		
+	} else {													//	Sinon déposer l'argent.
 		cout << "Combien voulez vous déposer ?" << endl;
 		cin >> amount;
 		addMoney(amount, actualAccount);
@@ -299,37 +357,52 @@ void dropMoneyFunc() {
 	tra.toId = actualAccount.id;
 	tra.type = deposit;
 	tra.amount = amount;
-	generateTransaction(tra);
+	generateTransaction(tra);											//	Génèrer une transaction.
 }
 
+/**
+	takeMoneyFunc (void)
+	Paramètre:
+	Prompt de retrait d'argent.
+**/
 void takeMoneyFunc() {
 	transaction tra;
 	
 	int amount = 0;
-	if(actualAccount.id == 0) {
+	if(actualAccount.id == 0) {											//	Si aucun compte connecté : Erreur.
 		cout << "Connectez vous à un compte d'abord." << endl;	
-	} else {
+	} else {													//	Sinon retirer l'argent.
 		cout << "Combien voulez vous retirer ?" << endl;
 		cin >> amount;
 		subMoney(amount, actualAccount);
 		actualAccount.amount=actualAccount.amount-amount;
 		cout << "Vous avez bien retirer " << amount << "€, vous avez donc " << actualAccount.amount << "€" << endl;
 	}
-	tra.fromId = actualAccount.id;
+	tra.fromId = actualAccount.id;												
 	tra.toId = actualAccount.id;
 	tra.type = withdraw;
 	tra.amount = amount;
-	generateTransaction(tra);
+	generateTransaction(tra);											//	Génèrer une transaction.
 }
 
+/**
+	checkAmountFunc (void)
+	Paramètre:
+	Prompt de vérification du solde.
+**/
 void checkAmountFunc() {
-	if(actualAccount.id == 0) {
+	if(actualAccount.id == 0) {											//	Si aucun compte connecté : Erreur.
 		cout << "Connectez vous à un compte d'abord." << endl;
-	} else {
+	} else {													//	Sinon retourner le solde.
 		cout << "Vous avez actuellement " << actualAccount.amount << "€" << endl;	
 	}
 }
 
+/**
+	getAccountFromId (account)
+	Paramètre: id (long long)
+	Retourne un compte depuis son id.
+**/
 account getAccountFromId(long long id) {
 	account acc;
 	
@@ -337,7 +410,7 @@ account getAccountFromId(long long id) {
 	
 	acc.id = id;
 	
-	string linePasscode = to_string(id)+"+P+";
+	string linePasscode = to_string(id)+"+P+";									//	Recherche du passcode.
 		unsigned int currentPasscode = 0;
 	
 		string line;
@@ -353,7 +426,7 @@ account getAccountFromId(long long id) {
 			}
 		}
 	
-	string lineAmount = to_string(id)+"+A+";
+	string lineAmount = to_string(id)+"+A+";									//	Recherche du solde.
 		unsigned int currentAmount = 0;
 	
 		string line1;
@@ -369,16 +442,21 @@ account getAccountFromId(long long id) {
 			}
 		}
 	
-	return acc;	
+	return acc;													//	Retourne un account.
 }
 
+/**
+	transferMoneyFunc (void)
+	Paramètre:
+	Prompt de transfert d'argent.
+**/
 void transferMoneyFunc() {
 	transaction tra;
 	long long toId;
 	int amount;
-	if(actualAccount.id == 0) {
-		cout << "Connectez vous à un compte d'abord." << endl;	
-	} else {
+	if(actualAccount.id == 0) {											//	Si aucun compte connecté : Erreur.
+		cout << "Connectez vous à un compte d'abord." << endl;		
+	} else {													//	Sinon transferer l'argent.		
 		cout << "Vers quel compte voulez-vous transferer l'argent ?" << endl;
 		cin >> toId;
 		cout << "De combien est le transfert ?" << endl;
@@ -389,64 +467,80 @@ void transferMoneyFunc() {
 		subMoney(amount, actualAccount);
 	}
 	
-	tra.fromId = actualAccount.id;
+	tra.fromId = actualAccount.id;												
 	tra.toId = toId;
 	tra.type = transfer;
 	tra.amount = amount;
-	generateTransaction(tra);
+	generateTransaction(tra);											//	Génèrer une transaction.
 	
 }
 
+/**
+	getChoice (void)
+	Paramètre:
+	Fonction de vérification du choix de menu.
+**/
 void getChoice() {
 	cin >> choice;
 	switch(choice) {
-		case createAccount:
+		case createAccount:											//	Action 1 : Création du compte.
 			createAccountFunc();
 			break;
 
-		case connectAccount:
+		case connectAccount:											//	Action 2 : Connexion au compte.
 			connectAccountFunc();
 			break;
 			
-		case dropMoney:
+		case dropMoney:												// 	Action 3 : Dépot d'argent.
 			dropMoneyFunc();
 			break;
 
-		case takeMoney:
+		case takeMoney:												//	Action 4 : Retrait d'argent.
 			takeMoneyFunc();
 			break;
 
-		case transferMoney:
+		case transferMoney:											//	Action 5 : Transfert d'argent.
 			transferMoneyFunc();
 			break;
 
-		case checkAmount:
+		case checkAmount:											//	Action 6 : Vérification du solde.
 			checkAmountFunc();
 			break;
 
-		case checkHistory:
+		case checkHistory:											//	Action 7 : Vérification de l'historique des transactions.
 			break;
 
-		case closeAccount:
+		case closeAccount:											//	Action 8 : Fermeture du compte.
 			break;
 
-		case close:
+		case close:												//	Action 9 : Arrêt du programme.
 			doExit = true;
 			break;
 
-		default:
+		default:												//	Sinon erreur.
 			cerr << "Choix incorrect." << endl;
 	}
 }
 
+/**
+	main (int)
+	Paramètre:
+	Fonction principale.
+**/
 int main()
 {
 	while(!doExit) {
-		  /**#ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif**/
+		/**
+		
+		Nettoyer la console à chaque étape.
+		
+		#ifdef _WIN32
+			system("cls");
+		#else
+			system("clear");
+		#endif
+		**/
+		
 		displayMenu();
 		getChoice();
 	}
